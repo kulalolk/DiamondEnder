6704ae449e6a8315157297cfcc6d6f25ca4665ef
kulalolk <kulalolk@gmail.com>
2014-05-05 15:21:42 -0400

    Tier 3 Creative Tab

---
+++ util/DiamondEnderTabT3.java
@@ -0,0 +1,23 @@
+package DiamondEnder.util;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+
+public class DiamondEnderTabT3 extends CreativeTabs {
+
+public DiamondEnderTabT3(int i, String tabDiamondEnderT3)
+{
+super(tabDiamondEnderT3);
+}
+
+@Override
+@SideOnly(Side.CLIENT)
+public Item getTabIconItem()
+{
+return Item.getItemFromBlock(Blocks.dirt);
+}
+
+}
