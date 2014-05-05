5f392a129d75d973920f7ebb0920b8969555b857
kulalolk <kulalolk@gmail.com>
2014-05-05 15:21:53 -0400

    Tier 2 Creative Tab

---
+++ util/DiamondEnderTabT2.java
@@ -0,0 +1,23 @@
+package DiamondEnder.util;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+
+public class DiamondEnderTabT2 extends CreativeTabs {
+
+public DiamondEnderTabT2(int i, String tabDiamondEnderT2)
+{
+super(tabDiamondEnderT2);
+}
+
+@Override
+@SideOnly(Side.CLIENT)
+public Item getTabIconItem()
+{
+return Item.getItemFromBlock(Blocks.dirt);
+}
+
+}
