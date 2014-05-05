377e4e6b7128997ca5329b19bda9815727d79e3f
kulalolk <kulalolk@gmail.com>
2014-05-05 15:23:11 -0400

    Diamond Ender Seeds

---
+++ items/DiamondEnderSeeds.java
@@ -0,0 +1,19 @@
+package DiamondEnder.items;
+ 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import DiamondEnder.DiamondEnder;
+ 
+public class DiamondEnderSeeds extends Item {
+
+	// Constructors
+	public DiamondEnderSeeds() {
+		
+		super();
+		this.setUnlocalizedName("DiamondEnderSeeds");
+		this.setTextureName("recipeplus:blueberry");
+		this.setCreativeTab(DiamondEnder.tabDiamondEnderT3);
+	}
+ 
+}
