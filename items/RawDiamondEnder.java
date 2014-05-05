2b6fdb367cf4494ad0ce007fe835eac05062dba3
kulalolk <kulalolk@gmail.com>
2014-05-05 15:22:58 -0400

    Raw Diamond Ender

---
+++ items/RawDiamondEnder.java
@@ -0,0 +1,20 @@
+package DiamondEnder.items;
+ 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import DiamondEnder.DiamondEnder;
+ 
+public class RawDiamondEnder extends Item {
+ 
+	// Constructors
+	public RawDiamondEnder() {
+		
+		super();
+		this.setUnlocalizedName("RawDiamondEnder");
+		this.setTextureName("diamondender:RawDiamondEnder");
+		this.setCreativeTab(DiamondEnder.tabDiamondEnderT3);
+ 
+	}
+ 
+}
