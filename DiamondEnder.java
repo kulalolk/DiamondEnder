eef81f0ba447185ef883ef5ca8a525fa6e835a73
kulalolk <kulalolk@gmail.com>
2014-05-08 13:38:59 -0400

    Update DiamondEnder.java

--- DiamondEnder.java
+++ DiamondEnder.java
@@ -52,11 +52,13 @@ public class DiamondEnder
     public static CreativeTabs tabDiamondEnderT1 = new DiamondEnderTabT1(CreativeTabs.getNextID(), "DiamondEnderT1");
     public static CreativeTabs tabDiamondEnderT2 = new DiamondEnderTabT2(CreativeTabs.getNextID(), "DiamondEnderT2");
     public static CreativeTabs tabDiamondEnderT3 = new DiamondEnderTabT3(CreativeTabs.getNextID(), "DiamondEnderT3");
+    public static CreativeTabs tabDiamondEnderT4 = new DiamondEnderTabT4(CreativeTabs.getNextID(), "DiamondEnderT4");
     
     //Materials    
     static ToolMaterial IronEnder = EnumHelper.addToolMaterial("IronEnder", 4, 2500, 15.0F, 6.0F, 30);
     static ToolMaterial GoldEnder = EnumHelper.addToolMaterial("GoldEnder", 4, 5000, 17.5F, 11.0F, 30);
-    static ToolMaterial DiamondEnder = EnumHelper.addToolMaterial("DiamondEnder", 4, -1, 20.0F, 16.0F, 30);
+    static ToolMaterial DiamondEnder = EnumHelper.addToolMaterial("DiamondEnder", 4, 10000, 20.0F, 16.0F, 30);
+    static ToolMaterial WitherEnder = EnumHelper.addToolMaterial("WitherEnder", 4, -1, 50.0F, 46.0F, 30);
 
     //Items & Blocks (Tier 1)
     public final static Block IronEnderCrop = new IronEnderCrop();
@@ -78,6 +80,18 @@ public class DiamondEnder
     public final static Block DiamondEnderCrop = new DiamondEnderCrop();
     public final static Item DiamondEnderSeeds = new DiamondEnderSeeds();
     public final static Item RawDiamondEnder = new RawDiamondEnder();
+    public final static Item DiamondEnderIngot = new DiamondEnderIngot();
+    public final static Item DiamondEnderEssence = new DiamondEnderEssence();
+    public final static Item DiamondEnderSword = new DiamondEnderSword(DiamondEnder);
+    public final static Item DiamondEnderPickaxe = new DiamondEnderPickaxe(DiamondEnder);
+    public final static Item DiamondEnderAxe = new DiamondEnderAxe(DiamondEnder);
+    public final static Item DiamondEnderShovel = new DiamondEnderShovel(DiamondEnder);
+    public final static Item DiamondEnderMultiTool = new DiamondEnderMultiTool(DiamondEnder);
+    
+        //Items & Blocks (Tier 4)
+    public final static Block WitherEnderCrop = new WitherEnderCrop();
+    public final static Item WitherEnderSeeds = new WitherEnderSeeds();
+    public final static Item RawWitherEnder = new RawWitherEnder();
     public final static Item DiamondEnderIngot = new DiamondEnderIngot();
     public final static Item DiamondEnderEssence = new DiamondEnderEssence();
     public final static Item DiamondEnderSword = new DiamondEnderSword(DiamondEnder);
@@ -124,31 +138,31 @@ public class DiamondEnder
 
     	//Language Registry (Tier 1)
     	LanguageRegistry.addName(IronEnderCrop, "Iron Ender Crop");
-    	LanguageRegistry.addName(IronEnderSeeds, "§eIron Ender Seeds" + "§8 {Tier 1}");
-    	LanguageRegistry.addName(RawIronEnder, "§eRaw Iron Ender" + "§8 {Tier 1}");
-    	LanguageRegistry.addName(IronEnderIngot, "§eIron Ender Ingot" + "§8 {Tier 1}");
-    	LanguageRegistry.addName(IronEnderSword, "§eIron Ender Sword" + "§8 {Tier 1}");
-    	LanguageRegistry.addName(IronEnderPickaxe, "§eIron Ender Pickaxe" + "§8 {Tier 1}");
+    	LanguageRegistry.addName(IronEnderSeeds, "ï¿½eIron Ender Seeds" + "ï¿½8 {Tier 1}");
+    	LanguageRegistry.addName(RawIronEnder, "ï¿½eRaw Iron Ender" + "ï¿½8 {Tier 1}");
+    	LanguageRegistry.addName(IronEnderIngot, "ï¿½eIron Ender Ingot" + "ï¿½8 {Tier 1}");
+    	LanguageRegistry.addName(IronEnderSword, "ï¿½eIron Ender Sword" + "ï¿½8 {Tier 1}");
+    	LanguageRegistry.addName(IronEnderPickaxe, "ï¿½eIron Ender Pickaxe" + "ï¿½8 {Tier 1}");
     	
     	//Language Registry (Tier 2)
     	LanguageRegistry.addName(GoldEnderCrop, "Gold Ender Crop");
-    	LanguageRegistry.addName(GoldEnderSeeds, "§eGold Ender Seeds" + "§8 {Tier 2}");
-    	LanguageRegistry.addName(RawGoldEnder, "§eRaw Gold Ender" + "§8 {Tier 2}");
-    	LanguageRegistry.addName(GoldEnderIngot, "§eGold Ender Ingot" + "§8 {Tier 2}");
-    	LanguageRegistry.addName(GoldEnderSword, "§eGold Ender Sword" + "§8 {Tier 2}");
-    	LanguageRegistry.addName(GoldEnderPickaxe, "§eGold Ender Pickaxe" + "§8 {Tier 2}");
+    	LanguageRegistry.addName(GoldEnderSeeds, "ï¿½eGold Ender Seeds" + "ï¿½8 {Tier 2}");
+    	LanguageRegistry.addName(RawGoldEnder, "ï¿½eRaw Gold Ender" + "ï¿½8 {Tier 2}");
+    	LanguageRegistry.addName(GoldEnderIngot, "ï¿½eGold Ender Ingot" + "ï¿½8 {Tier 2}");
+    	LanguageRegistry.addName(GoldEnderSword, "ï¿½eGold Ender Sword" + "ï¿½8 {Tier 2}");
+    	LanguageRegistry.addName(GoldEnderPickaxe, "ï¿½eGold Ender Pickaxe" + "ï¿½8 {Tier 2}");
     	
     	//Language Registry (Tier 3)
     	LanguageRegistry.addName(DiamondEnderCrop, "Diamond Ender Crop");
-    	LanguageRegistry.addName(DiamondEnderSeeds, "§eDiamond Ender Seeds" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(RawDiamondEnder, "§eRaw Diamond Ender" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderEssence, "§eDiamond Ender Essence" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderIngot, "§eDiamond Ender Ingot" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderSword, "§eDiamond Ender Sword" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderPickaxe, "§eDiamond Ender Pickaxe" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderShovel, "§eDiamond Ender Shovel" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderAxe, "§eDiamond Ender Axe" + "§8 {Tier 3}");
-    	LanguageRegistry.addName(DiamondEnderMultiTool, "§eDiamond Ender Multi Tool" + "§8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderSeeds, "ï¿½eDiamond Ender Seeds" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(RawDiamondEnder, "ï¿½eRaw Diamond Ender" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderEssence, "ï¿½eDiamond Ender Essence" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderIngot, "ï¿½eDiamond Ender Ingot" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderSword, "ï¿½eDiamond Ender Sword" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderPickaxe, "ï¿½eDiamond Ender Pickaxe" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderShovel, "ï¿½eDiamond Ender Shovel" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderAxe, "ï¿½eDiamond Ender Axe" + "ï¿½8 {Tier 3}");
+    	LanguageRegistry.addName(DiamondEnderMultiTool, "ï¿½eDiamond Ender Multi Tool" + "ï¿½8 {Tier 3}");
 
     	// register tileentities
  
