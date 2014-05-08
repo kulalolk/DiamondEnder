1565610867d8bca6409f984814f6215fb2011325
kulalolk <kulalolk@gmail.com>
2014-05-08 13:34:55 -0400

    Create DiamondEnderTabT4.java

---
+++ util/DiamondEnderTabT4.java
@@ -0,0 +1,22 @@
+package DiamondEnder.util;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+
+public class DiamondEnderTabT4 extends CreativeTabs {
+
+public DiamondEnderTabT4(int i, String tabDiamondEnderT4)
+{
+super(tabDiamondEnderT4);
+}
+
+@Override
+@SideOnly(Side.CLIENT)
+public Item getTabIconItem()
+{
+return Item.getItemFromBlock(Items.netherStar);
+  }
+}
